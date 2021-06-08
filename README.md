221af06ca51659f47ef36ca873455d28300368cf
TheTeejers <tjloughry@gmail.com>
2021-06-08 15:40:53 -0500

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # sc_tunnel_data.py
+
+sc_tunnel_data. is a Python library for getting Sauce Labs Sauce Connect Data.
+
+## Usage
+
+in CMD line:
+
+python sc_tunnel_data.py <sauce_labs_username> <sauce_labs_acces_key> <tuennel_id>sc_tunnel_data
---
+++ sc_tunnel_data.py
@@ -0,0 +1,54 @@
+import sys
+from requests.auth import HTTPBasicAuth
+import os, requests
+from termcolor import (colored)
+
+SAUCE_USERNAME = os.environ["SAUCE_USERNAME"]
+SAUCE_ACCESS_KEY = os.environ["SAUCE_ACCESS_KEY"]
+
+#First arg in command is sc_tunnel_data.py
+
+#seconf arg is the username
+user = sys.argv[1]
+
+#Third arg is the user's access key
+user_key = sys.argv[2]
+
+#fourth arg is the tunnel ID
+tunnel_id = sys.argv[3]
+
+
+dataCenter = ['us-west-1', 'eu-central-1', 'us-east-1', 'apac-southeast-1']
+region = 0
+
+response = requests.get("https://api."+ dataCenter[region] +".saucelabs.com/rest/v1/" + user +"/tunnels/" + tunnel_id + "" , auth=HTTPBasicAuth(SAUCE_USERNAME, SAUCE_ACCESS_KEY))
+
+if int(response.status_code) != 200:
+	print(response.status_code)
+	print(dataCenter[region])
+	region =+ 1
+	print(dataCenter[region])
+
+	response = requests.get("https://api."+ dataCenter[region] +".saucelabs.com/rest/v1/" + user +"/tunnels/" + tunnel_id + "" , auth=HTTPBasicAuth(SAUCE_USERNAME, SAUCE_ACCESS_KEY))
+
+
+print (colored("Release: ", 'green'), response.json()['metadata']['release'])
+
+if str(response.json()['status']) == 'running':
+	print(colored("Status: ", 'green'), colored(response.json()['status'], 'green', attrs=['blink', 'underline']))
+else:
+	print(colored("Status: ", 'green'), colored(response.json()['status'], 'red'))
+	print(colored("User Shut Down: ", 'green'), colored(response.json()['user_shutdown'], 'red'))
+
+
+if str(response.json()['tunnel_identifier']) != '':
+	print (colored("Tunnel Identifier: ", 'green'), response.json()['tunnel_identifier'])
+else:
+	print (colored("Tunnel Identifier: ", 'green'), colored("none", 'red'))
+
+if str(response.json()['shared_tunnel']) == 'true':
+	print (colored("Shared Tunnel: ", 'green'), response.json()['shared_tunnel'])
+else:
+	print (colored("Shared Tunnel: ", 'green'), colored(response.json()['shared_tunnel'], 'red'))
+
+print (colored("Command Arguments: ", 'green'), response.json()['metadata']['command_args'])
